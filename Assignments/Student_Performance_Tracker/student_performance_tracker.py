352aa28bd59611b5e24e1e76774366422aa63d00
Asad777 <muhammadasad7.7.2007@gmail.com>
2024-10-18 00:49:25 +0500

    input_structure done

---
+++ Assignments/Student_Performance_Tracker/student_performance_tracker.py
@@ -0,0 +1,8 @@
+# Add students.
+# Store their grades for different subjects.
+# Calculate the student’s average score.
+# Check if students are passing or failing based on a threshold.
+# Calculate the class's average score.
+# Display student performance in a structured manner.
+
+
